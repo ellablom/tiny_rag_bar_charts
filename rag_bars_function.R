4860690ce5fe6bec4ba3e9dd3e5b480cbc4fe2be
ellablom <ellablom@gmail.com>
2026-04-07 03:04:18 +0100

    Added function to make graph rows and function to break up text

--- rag_bars_function.R
+++ rag_bars_function.R
@@ -1,7 +1,12 @@
+# Function to add line breaks to labels
+line_break <- function(x,...){
+  gsub('\\s','\n',x)
+}
+
 # Function to make individual bar charts
 rag_bars <- function(data) {
   # Make graph
-  ggplot(data, aes(x = rag, y = count, fill = rag), group = 1) +
+  ggplot(data, aes(x = rag, y = count, fill = rag)) +
     geom_bar(stat = "identity") +
     # Set colours 
     scale_fill_manual(values = c("red" = "#D2222D",
@@ -9,18 +14,16 @@ rag_bars <- function(data) {
                                        "green" = "#238823")) +
     # Remove unwanted elements
     theme(legend.position = "none", 
-          strip.text.x = element_blank(),
           panel.grid = element_blank(),
           panel.background = element_blank(),
           axis.title.x = element_blank(),
-          axis.title.y = element_blank(),
           axis.ticks.x = element_blank(),
           axis.text.x = element_blank(),
-          plot.margin = margin(0, 0, -.2, 0, "cm"),
           # Format ticks and text elements
           axis.ticks.y = element_line(size = .1),
           axis.text.y = element_text(size = 5),
-          plot.caption = element_text(vjust = 7.5, hjust = 0, size = 5)
+          plot.caption = element_text(vjust = 7.5, hjust = 0, size = 5),
+          strip.background = element_blank()
     ) +
     # Format y axis labels
     scale_y_continuous(breaks = seq(0, 20, by = 5), limits = c(0, 20))
@@ -32,6 +35,22 @@ rag_bars_ind <- function(data) {
   total_values <- sum(data$count)
   # Make plots
   rag_bars(data) +
+    # Remove axes and add margin
+    theme(axis.title.y = element_blank(),
+          plot.margin = margin(0, 0, -.2, 0, "cm"),
+          strip.text.x = element_text(size = 5)
+    ) +
     # Add annotation
     labs(caption = paste("Observations:", total_values))
+}
+
+# Function to make a row of faceted bar charts
+rag_bars_row <- function(data){
+  comp_num = n_distinct(comp_data$sub_component)
+  rag_bars(data) +
+    theme(strip.text.x = element_text(size = 5)
+          ) +
+    # Facet wrap
+    facet_wrap(~sub_component, ncol = comp_num, labeller = label_wrap_gen(width = 10)) +
+    force_panelsizes(total_width = unit(comp_num * 1.5, "cm"), total_height = unit(2, "cm"))
 }
